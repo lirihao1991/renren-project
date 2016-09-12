28f642a961dd090f17d362572836ce2257e7a6f1
rihao.li <rihao.li@renren-inc.com>
2016-09-12 15:10:04 +0800

    添加切换功能

--- tools/bin/test.js
+++ tools/bin/test.js
@@ -3,7 +3,8 @@
 var path = require('path'),
     initProject = require('../lib/initProject'),
     importLib = require('../lib/importLib'),
-    addtmp = require('../lib/addtmp')
+    addtmp = require('../lib/addtmp'),
+    switchProject = require('../lib/switchProject');
 
 
 switch (process.argv[2]){
@@ -14,4 +15,6 @@ switch (process.argv[2]){
             importLib(process.argv[3]);
     case 'addtmp':
             addtmp(process.argv[3]);
+    case 'co':
+            switchProject(process.argv[3]);
 }
