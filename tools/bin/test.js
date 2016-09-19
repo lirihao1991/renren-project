4a74d679dc967c92b8fe4ae5872e5b86e8760bec
rihao.li <rihao.li@renren-inc.com>
2016-09-19 19:02:02 +0800

    修改webpack config模版和增加rebuild api

--- tools/bin/test.js
+++ tools/bin/test.js
@@ -6,6 +6,7 @@ var path = require('path'),
     addtmp = require('../lib/addtmp'),
     switchProject = require('../lib/switchProject'),
     showlist = require('../lib/list');
+    rebuild = require('../lib/rebuild');
 
 
 switch (process.argv[2]){
@@ -23,5 +24,8 @@ switch (process.argv[2]){
             break;
     case 'list':
             showlist();
+            break;
+    case 'rebuild':
+            rebuild();
             break;
 }
